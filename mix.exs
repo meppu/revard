6059031b61d2ee51360ffd84ef558eac168dbb9d
meppu <meppu@proton.me>
2023-06-18 01:23:29 +0300

    update to v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Revard.MixProject do
   def project do
     [
       app: :revard,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
