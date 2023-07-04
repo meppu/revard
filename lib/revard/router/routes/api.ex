c8c52c69ed7385eb42b0ae59a8610b03bbc2f3e1
meppu <meppu@proton.me>
2023-07-04 13:22:26 +0300

    switch to corsica

--- lib/revard/router/routes/api.ex
+++ lib/revard/router/routes/api.ex
@@ -8,11 +8,17 @@ defmodule Revard.Router.Routes.API do
   alias Revard.API
   alias Revard.Router.Utils
 
-  plug(:match)
-  plug(CORSPlug)
-  plug(:dispatch)
+  plug :match
 
-  forward("/users", to: API.Users)
+  plug Corsica,
+    origins: "*",
+    max_age: 600,
+    allow_methods: :all,
+    allow_headers: :all
+
+  plug :dispatch
+
+  forward "/users", to: API.Users
 
   match _ do
     Utils.unknown_route(conn)
